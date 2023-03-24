742909f7be0d8c97ae946bbd981a05712d32de6f
Marieevangeline <medelabroise@juniorisep.com>
2023-03-24 09:45:01 +0100

    morpion

---
+++ src/main/java/com/isep/tictactoe/console/Color.java
@@ -0,0 +1,12 @@
+package com.isep.tictactoe.console;
+
+public enum Color {
+    BLUE("\033[94m"),
+    RED("\033[31m"),
+    GREEN("\033[92m"),
+    BACK("\033[0m");
+    final String color;
+    Color(String color) {
+        this.color = color;
+    }
+}
