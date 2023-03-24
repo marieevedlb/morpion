eaf3191a90cb839c61073943a3a7ce2d52a7b852
Marieevangeline <medelabroise@juniorisep.com>
2023-03-24 09:28:12 +0100

    Initial commit

---
+++ src/main/java/com/isep/tictactoe/core/Player.java
@@ -0,0 +1,10 @@
+
+    public class Player {
+        final int color;
+        final Symbol symbol;
+        public Player(int color, Symbol symbol) {
+            this.color = color;
+            this.symbol = symbol;
+        }
+    }
+
---
+++ src/main/java/com/isep/tictactoe/core/Symbol.java
@@ -0,0 +1,18 @@
+import java.util.InputMismatchException;
+public enum Symbol {
+    NAUGHT("o"),
+    CROSS("x");
+    public final String symbol;
+    Symbol(String symbol) {
+        this.symbol = symbol;
+    }
+    public static Symbol getSymbol(int color){
+        switch(color){
+            case -1:
+                return Symbol.NAUGHT;
+            case 1:
+                return Symbol.CROSS;
+        }
+        throw new InputMismatchException();
+    }
+}
